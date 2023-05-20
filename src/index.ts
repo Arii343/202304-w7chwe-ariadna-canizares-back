237373a5ab614be54b33f1455e95bf952d5585d6
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-20 21:47:54 +0200

    Create database connection and loadEnvironment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import express from "express";
+import "./loadEnvironment.js";
+import chalk from "chalk";
+import createDebug from "debug";
+import { app } from "./server/index.js";
+import connectToDataBase from "./database/connectToDataBase.js";
 
-export const app = express();
+const debug = createDebug("social-network-api:root");
+
+const port = process.env.PORT ?? 4000;
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongoDbConnection) {
+  debug(chalk.red("Missing environment variables"));
+  process.exit(1);
+}
+
+app.listen(port, () => {
+  debug(chalk.green(`Server up in http://localhost:${port}`));
+});
+
+try {
+  await connectToDataBase(mongoDbConnection);
+  debug(chalk.green("Connected to database"));
+} catch (error: unknown) {
+  debug(chalk.red(`Error connecting to database: ${(error as Error).message}`));
+}
